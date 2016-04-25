47473d7fb1b3f5a6b5271ba36c1f8178967b764b
tokenturtle98 <webbj98@gmail.com>
2016-04-24 21:28:06 -0500

    Added files via upload
    v0.0.2

--- SanityMeter.js
+++ SanityMeter.js
@@ -3,24 +3,14 @@
  */
 var insanity = 0;
 function sanityTest(){
-	var lastsleep;
-	lastsleep = window.prompt("How many hours ago have you slept?");
-	var lastsleepP = parseInt(lastsleep); //turns the string into an int
-	if(lastsleepP < 6){
-		//no insanity is added
-	}
-	else if(lastsleepP > 6 && lastsleepP < 12){
-		addSanity(2); //adds to insanity
-		
-	}
-	
-			
-	
-	
+	sleepQ();
+	daysQ();
 }
 
 function getError(){
-	
+	window.alert("That's an invalid input.");
+	insanity += 2;
+	return insanity;
 }
 
 function addSanity(i){  //adds the amount passed to the total insanity
@@ -30,3 +20,38 @@ function getSanity(insanity) //returns insanity as an int
 {
 	return insanity;
 	}
+///////////////////////////Questions Methods////////////////
+//sleep//
+function sleepQ(){
+	var lastsleep;
+	lastsleep = window.prompt("How many hours ago have you slept?");
+	var lastsleepP = parseInt(lastsleep); //turns the string into an int
+	
+	if(lastsleepP < 8 && lastsleepP >=0){
+		//no insanity is added
+	}
+	else if(lastsleepP >= 8 && lastsleepP < 16){
+		addSanity(2); //adds to insanity
+	}
+	else if (lastsleepP >= 16 && lastsleepP < 24 ){addSanity(3);}
+	else if (lastsleepP >=24){addSanity(5);}
+	else{
+		getError();
+		sleepQ(); //uses recursion so that the method restarts if wrong input
+	}
+}
+//length of apocalypse//
+function daysQ(){
+	var days = window.prompt("Approxmately how many days have their been zombies around?");
+	var daysP = parseFloat(days);
+	if (daysP >0){
+		var add = days/100;
+		addSanity(add); //should probably make it so that it starts off high, gets less, than goes back up
+	}
+	else{
+		getError();
+		daysQ();
+	}
+	
+}
+
