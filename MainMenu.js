dd2d916280bf02c3815fc9e0e2fa3844d4a8ab6b
Kevin Jin <kevin.defan@gmail.com>
2016-05-09 12:31:05 -0500

    Add switch-case structure

--- MainMenu.js
+++ MainMenu.js
@@ -4,12 +4,28 @@
 function MainMenu()
 {
 	var choice = window.prompt("Choose an option: 1. Sanity Test  2. Personalized Guide  3. First Aid")
-	if(choice == "1" || "sanity test" || "Sanity Test")
+
+	switch (choice) {
+		case 1:
+			sanityTest();
+			break;
+		/*case 2:
+			personalGuide();
+			break; */
+		case 3:
+			firstAid();
+			break;
+	}
+	
+	/* if(choice == "1" || "sanity test" || "Sanity Test")
 		{
 			sanityTest();
 		}
+	*/
+
 }
+
 function getGreeting()
 {
-	window.alert("Hello! I'm your friendly neighborhood chatbot her to help you with your zombie troubles.")
+	window.alert("Hello! I'm your friendly neighborhood chatbot, here to help you with your zombie troubles.")
 }
