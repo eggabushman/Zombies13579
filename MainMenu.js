2bda15a1439db0866a8801692cd9978820a001ae
Kevin Jin <kevin.defan@gmail.com>
2016-05-09 12:52:00 -0500

    Looped and improved switch-case

--- MainMenu.js
+++ MainMenu.js
@@ -3,19 +3,31 @@
  */
 function MainMenu()
 {
-	var choice = window.prompt("Choose an option: 1. Sanity Test  2. Personalized Guide  3. First Aid")
+	var exitLoop = true;
 
-	switch (choice) {
-		case 1:
-			sanityTest();
-			break;
-		/*case 2:
-			personalGuide();
-			break; */
-		case 3:
-			firstAid();
-			break;
+	while (exitLoop) 
+	{
+		var choice = window.prompt("Choose an option: \n1. Sanity Test  \n2. Personalized Guide  \n3. First Aid \n4. Quit");
+
+		switch (choice) 
+		{
+			case 1:
+				sanityTest();
+				exitLoop = false;
+			/*case 2:
+				personalGuide();
+				exitLoop = false; */
+			case 3:
+				firstAid();
+				exitLoop = false;
+			case 4:
+				break;
+			default:
+				window.alert("That's not an option!");
+				exitLoop = true; 
+		}
 	}
+
 	
 	/* if(choice == "1" || "sanity test" || "Sanity Test")
 		{
@@ -25,7 +37,7 @@ function MainMenu()
 
 }
 
-function getGreeting()
+function getGreeting() 
 {
-	window.alert("Hello! I'm your friendly neighborhood chatbot, here to help you with your zombie troubles.")
+	window.alert("Hello! I'm your friendly neighborhood chatbot, here to help you with your zombie troubles.");
 }
