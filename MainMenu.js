24752172b9fb263173f295cb1878620b09412c46
tokenturtle98 <webbj98@gmail.com>
2016-05-12 17:52:42 -0500

    Add files via upload

---
+++ MainMenu.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+function MainMenu()
+{
+	var exitLoop = true;
+
+	while (exitLoop) 
+	{
+		var choice = window.prompt("Choose an option: \n1. Sanity Test   First Aid   Quit");
+		window.resizeTo(100);
+		switch (choice) 
+		{
+			case "Sanity Test":
+				
+				sanityTest();
+				exitLoop = false;
+			/*case 2:
+				personalGuide();
+				exitLoop = false; */
+			case "First Aid":
+				firstAid();
+				exitLoop = false;
+			case  "Quit": 
+				window.close();
+				exitLoop = false;
+				break;
+			default:
+				window.alert("That's not an option!");
+				exitLoop = false; 
+		}
+	}
+
+	
+	/* if(choice == "1" || "sanity test" || "Sanity Test")
+		{
+			sanityTest();
+		}
+	*/
+
+}
+
+function getGreeting() 
+{
+	window.alert("Hello! I'm your friendly neighborhood chatbot, here to help you with your zombie troubles.");
+}
